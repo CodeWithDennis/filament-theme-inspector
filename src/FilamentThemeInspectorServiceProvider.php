14615ee0091d708d59ae6a047e234398fd1ba75b
Dennis Elsinga <personal.elsinga@gmail.com>
2024-10-18 15:17:08 +0200

    Cleanup

--- src/FilamentThemeInspectorServiceProvider.php
+++ src/FilamentThemeInspectorServiceProvider.php
@@ -18,11 +18,6 @@ class FilamentThemeInspectorServiceProvider extends PackageServiceProvider
 
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package->name(static::$name)
             ->hasInstallCommand(function (InstallCommand $command) {
                 $command
