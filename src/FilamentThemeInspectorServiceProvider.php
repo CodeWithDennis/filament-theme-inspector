fd94b3c76f1079442bd00134f2214e4cb4523d00
CodeWithDennis <CodeWithDennis@users.noreply.github.com>
2024-10-18 08:00:42 +0000

    Fix styling

--- src/FilamentThemeInspectorServiceProvider.php
+++ src/FilamentThemeInspectorServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace CodeWithDennis\FilamentThemeInspector;
 
+use CodeWithDennis\FilamentThemeInspector\Commands\FilamentThemeInspectorCommand;
+use CodeWithDennis\FilamentThemeInspector\Testing\TestsFilamentThemeInspector;
 use Filament\Support\Assets\AlpineComponent;
 use Filament\Support\Assets\Asset;
 use Filament\Support\Assets\Css;
@@ -13,8 +15,6 @@ use Livewire\Features\SupportTesting\Testable;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use CodeWithDennis\FilamentThemeInspector\Commands\FilamentThemeInspectorCommand;
-use CodeWithDennis\FilamentThemeInspector\Testing\TestsFilamentThemeInspector;
 
 class FilamentThemeInspectorServiceProvider extends PackageServiceProvider
 {
